8043718606519ea6a865f07dad516544f08ad1ff
Francesco Zanini <francesco@zanini.me>
2020-05-15 19:08:48 +0200

    Fix build and tests

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,1 +1,11 @@
-let sources = import ./sources.nix; in import sources.nixpkgs { }
+let
+  sources = import ./sources.nix;
+  varnishFix = self: super: {
+    varnish = super.varnish.overrideAttrs (old: {
+      postPatch = (old.postPatch or "") + ''
+        substituteInPlace bin/varnishtest/vtc_main.c --replace "/bin/" ""
+        substituteInPlace bin/varnishtest/vtc_process.c --replace "/bin/" ""
+      '';
+    });
+  };
+in import sources.nixpkgs { overlays = [ varnishFix ]; }
