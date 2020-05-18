d5b99b6655de0f4615b3a07e2e979fb184d15ab3
Francesco Zanini <francesco@zanini.me>
2020-05-18 11:46:33 +0200

    Use submitted PR for patching varnish

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -2,10 +2,13 @@ let
   sources = import ./sources.nix;
   varnishFix = self: super: {
     varnish = super.varnish.overrideAttrs (old: {
-      postPatch = (old.postPatch or "") + ''
-        substituteInPlace bin/varnishtest/vtc_main.c --replace "/bin/" ""
-        substituteInPlace bin/varnishtest/vtc_process.c --replace "/bin/" ""
-      '';
+      patches = (old.patches or [ ]) ++ [
+        (super.fetchpatch {
+          url =
+            "https://patch-diff.githubusercontent.com/raw/varnishcache/varnish-cache/pull/3327.diff";
+          sha256 = "1lnfngjds1iyw4nml17jkhj5k0ny3cqni9ikgrbbm186l528l91f";
+        })
+      ];
     });
   };
 in import sources.nixpkgs { overlays = [ varnishFix ]; }
